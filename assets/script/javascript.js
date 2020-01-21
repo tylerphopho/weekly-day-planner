e7bb1cd6393f0d0d1fd7d3babd66d8be52bebdcd
Tyler Pho <owner@Tyler-Phos-MacBook-Pro.local>
2020-01-20 20:28:06 -0700

    created time, textarea, savebutton

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,8 +1,77 @@
-var current = $("#current-day");
-
 $(document).ready(function(){
-    var m = moment();
-    moment().format("dddd , MMMM Do YYYY");
-    console.log(m.toString());
+    let currentDay = $("#current-day")
+    currentDay.text(moment().format("LLLL"))
+
+    let timeblocks = $("#timemarkers")
+
+    var currentHour = parseInt(moment().format("H"))
+    console.log(currentHour)
+
+    var times = [
+        {time: "9 AM", value: '9'},
+        {time: "10 AM", value: '10'},
+        {time: "11 AM", value: '11'},
+        {time: "12 PM", value: '12'},
+        {time: "1 PM", value: '13'},
+        {time: "2 PM", value: '14'},
+        {time: "3 PM", value: '15'},
+        {time: "4 PM", value: '16'},
+        {time: "5 PM", value: '17'},
+    ]
+    for (var i = 0; i < times.length; i++) {
+        savedText = localStorage.getItem(i) || ""
+        
+        // Creates a div tag for the time
+        var timeRow = $("<div>");
+        timeRow.addClass("<row>");
+        timeblocks.append(timeRow);
+        // Appends time, the text area, and button.
+        timeRow.append(hourRow, textRow, button);
+
+        // Creates div to hold current hour
+        var hourRow = $("<div>");
+        hourRow.addClass("time col-md-1");
+        hourRow.html(`<p class="hour-text">${times[i].time}</p>`);
+
+        // Creates div to hold text area
+        var textRow = $("<div>");
+        textRow.addClass("col-md-10");
+        textRow.attr("value", times[i].milTime);
+        var textArea = $("<textarea>");
+        textArea.text(savedText);
+        textRow.append(textArea);
+
+        // Creates button to save the text area
+        var button = $("<div>");
+        button.addClass("col-md-1");
+        button.append(saveButton);
+
+        var saveButton = $("<button>");
+        saveButton.addClass("saveBtn");
+        saveButton.html(`<i class="fas a-lock"></i>`);
+    }
+
+    $(".saveBtn").click(function(){
+        var text = $(this).parent().find("textArea").val()
+        var displayHour = $(this).parent.find("textArea").attr("id");
+        localStorage.setItem(displayHour, text);
+        $(this).parent().find("textArea").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    })
+
+    function changeColor() {
+        var rowColor = $(".col-md-10");
+        for (let j = 0; j < rowColor.length; j++) {
+          var currentDiv = $(rowColor[j]);
+          if (currentDiv.attr("value") > currentHour) {
+            currentDiv.addClass("future");
+          } else if (currentDiv.attr("value") < currentHour) {
+            currentDiv.addClass("past");
+          } else {
+            currentDiv.addClass("present");
+          }
+        }
+      }
+      // Call the changeColor function
+      changeColor();
 })
 
